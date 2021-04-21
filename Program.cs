4dec755fc352d205d566a5260bd37126ed914942
Andres Sainz <andresmsainz@gmail.com>
2021-04-20 22:48:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int_Serial_No = 100;
+            string str_Acc_No = "";
+            float flt_Any_No = 3.5f;
+
+            str_Acc_No = int_Serial_No.ToString();
+
+            Console.WriteLine("str_Acc_No = " + str_Acc_No);
+
+            flt_Any_No = int_Serial_No;
+            Console.WriteLine("flt_Any_No = " + flt_Any_No);
+
+            flt_Any_No = 3.5f;
+            int_Serial_No = Convert.ToInt32(flt_Any_No);
+            Console.WriteLine("New int_Serial_No = " + int_Serial_No);
+
+        }
+    }
+}
